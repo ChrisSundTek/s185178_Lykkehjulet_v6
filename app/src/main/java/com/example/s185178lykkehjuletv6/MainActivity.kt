38c40388a4cb6fc6bec05e5811f4f8281d392975
Christoffer <s185178@student.dtu.dk>
2021-11-27 11:09:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/s185178lykkehjuletv6/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.s185178lykkehjuletv6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// My 6th Github file, others has been deleted because they kept crashing
+// and give me errors for no reason, so couldn't figure out why so had to delete and start over
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
